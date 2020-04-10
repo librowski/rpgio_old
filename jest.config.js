f3ce7e2a127ad42d1d6b984f5e5e39412b8bf84d
Jan Librowski <janlibrowski@gmail.com>
2020-04-10 12:03:48 +0200

    Setup ci (#6)
    * Setup jest and enzyme
    
    * Add types for lodash
    
    * Fix main.js path in electron-builder configuration
    
    * Fix main.js path in electron-builder configuration
    
    * Create unpackaged buld on dist command
    
    * Don't publish the app on dist command

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
     coverageDirectory: 'coverage',
     collectCoverageFrom: ['src/**/*.{ts,tsx}'],
     setupFilesAfterEnv: ['./src/test/jest-setup.ts'],
+    roots: ['src'],
+    testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
 };
