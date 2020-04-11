c7771f427ad41b8f331e5ca7e3d8614adde560f0
Jan Librowski <janlibrowski@gmail.com>
2020-04-11 17:52:47 +0200

    Add basic routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react';
 import { Provider } from './providers/Provider';
 import { Main } from './views/Main/Main';
+import { Route, Switch } from 'react-router-dom';
+import { SETTINGS_PATH } from './views/navigation';
 
 export const App: React.FC = () => (
     <Provider>
-        <Main />
+        <Switch>
+            <Route path={SETTINGS_PATH} />
+            <Route component={Main} />
+        </Switch>
     </Provider>
 );
