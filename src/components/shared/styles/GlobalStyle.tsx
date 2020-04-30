c9dd90a16af7e40e96edeccd93db84204a4e5f06
Jan Librowski <janlibrowski@gmail.com>
2020-04-30 17:26:36 +0200

    Fix app flickering issue

--- src/components/shared/styles/GlobalStyle.tsx
+++ src/components/shared/styles/GlobalStyle.tsx
@@ -1,21 +1,8 @@
 import { createGlobalStyle } from 'styled-components';
-import DMSans from '../../../assets/fonts/DMSans-Regular.ttf';
-import DMSansMedium from '../../../assets/fonts/DMSans-Medium.ttf';
 import { getColor } from './theme';
+import './fonts.css';
 
 export const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: 'DM Sans';
-        src: url(${DMSans});
-        font-weight: normal;
-    }
-
-    @font-face {
-        font-family: 'DM Sans';
-        src: url(${DMSansMedium});
-        font-weight: 500;
-    }
-
     html, body, #root {
         margin: 0;
         padding: 0;
